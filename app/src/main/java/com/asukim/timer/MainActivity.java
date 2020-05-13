503f35d6c607f0ddc21649492b6d2d569a652fa8
asuKim <asukim1022@gmail.com>
2020-05-13 22:54:49 +0900

    Timer

---
+++ app/src/main/java/com/asukim/timer/MainActivity.java
@@ -0,0 +1,56 @@
+package com.asukim.timer;
+
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    int timer_sec;
+    int count;
+    private TimerTask second;
+    private TextView timer_text;
+    private final Handler handler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        testStart();
+    }
+
+    public void testStart() {
+        timer_text = (TextView) findViewById(R.id.timer);
+        timer_sec = 0;
+        count = 0;
+
+        second = new TimerTask() {
+
+            @Override
+            public void run() {
+                Log.i("Test", "Timer start");
+                Update();
+                timer_sec++;
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(second, 0, 1000);
+    }
+
+    protected void Update() {
+        Runnable updater = new Runnable() {
+            public void run() {
+                timer_text.setText(timer_sec + "초");
+            }
+        };
+        handler.post(updater);
+    }
+}
